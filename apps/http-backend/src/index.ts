c791a50ef4136002e920bc1dd25c54b9d1fb8780
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-02-06 13:06:24 +0530

    Added Prisma Defined Schema migrated

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,18 @@
 import express, { json } from "express";
+import { client } from "@repo/db/client"
 
 const app = express();
 app.use(express.json());
 
+
 app.post("/signup", (req, res) => {
 
 })
+
+app.post("/signup", (req, res) => {
+
+})
+
 
 const PORT = 3001;
 
