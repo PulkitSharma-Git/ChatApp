6da6e1e76eca28194387c2bf313c243f453955e0
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-02-06 14:31:23 +0530

    sign up sign in endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,71 @@
 import express, { json } from "express";
-import { client } from "@repo/db/client"
+import { client } from "@repo/db/client";
+import jwt from "jsonwebtoken";
+
 
 const app = express();
 app.use(express.json());
 
 
-app.post("/signup", (req, res) => {
+
+interface UserCreateInput {
+    email: string;
+    password: string;
+    name: string;
+}
+
+interface UserWhereInput {
+    email: string,
+    password: string
+}
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const newUser: UserCreateInput = {
+        email: username,
+        password,
+        name
+    }
+
+    await client.user.create({
+        data: newUser
+    })
+
+    res.json({
+        message: "Signed up successuflly"
+    })
+
 
 })
 
-app.post("/signup", (req, res) => {
+app.post("/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    const searchUser: UserWhereInput = {
+        email: username,
+        password
+    }
+
+    const user = await client.user.findFirst({
+        where: searchUser
+    })
+
+    if(!user) {
+        res.json({
+            message: "User not found. Please Sign In !"
+        })
+        return;
+    }
+
+    const token = jwt.sign(user , "jwtSecret"); //JWT_SECRET is hard coded make it a import which is not pushed
+
+    res.json({
+        token
+    })
 })
 
 
